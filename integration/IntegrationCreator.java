0bd181d207401da4e1d088195a7b7a25718d74de
Andreas <andreas.a.98@hotmail.com>
2025-05-31 16:19:09 +0200

    Update IntegrationCreator.java

--- integration/IntegrationCreator.java
+++ integration/IntegrationCreator.java
@@ -1,20 +1,32 @@
 package se.kth.iv1350.integration;
 
+/**
+ * This class is responsible for the creation of the three external systems.
+ */
 public class IntegrationCreator {
     private ExternalInventorySystem extInvSys = new ExternalInventorySystem();
     private ExternalAccountingSystem extAccSys = new ExternalAccountingSystem ();
     private DiscountDatabase discountDB = new DiscountDatabase();
 
+     /**
+     * @return passes the instantiated ExternalInventorySystem back.
+     */
     public ExternalInventorySystem getExternalInventorySystem() {
 
         return extInvSys;
     }
 
+    /**
+     * @return passes the instantiated ExternalAccountingSystem back.
+     */
     public ExternalAccountingSystem getExternalAccountingSystem () {
 
         return extAccSys;
     }
 
+    /**
+     * @return passes the instantiated DiscountDatabase back.
+     */
     public DiscountDatabase getDiscountDatabase() {
         
         return discountDB;
